ec69a5933c80db85e00b14096cedb03220fbfe6d
anydv1 <anupamayd@gmail.com>
2018-12-27 16:37:33 +1030

    lets start

---
+++ routes/users.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/users/login', function(req, res, next) {
+  res.render('/login', { title: 'User Login' });
+});
+router.get('/users/signup', function(req, res, next) {
+  res.render('/signup', { title: 'New User Registration' });
+
+});
+
+module.exports = router;
